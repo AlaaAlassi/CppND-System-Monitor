4ebb8356beefcb9f5607614d85b151518d00ec00
alaa alassi <alaa.alasi1989@gmail.com>
2020-09-21 22:29:45 +0200

    cleaning main

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,16 @@
 
 int main() {
  std::vector<Process> processes;
-  int n = 10;
-  std::vector<int> pidVector = LinuxParser::Pids();
-        for (auto pid:pidVector){
-         processes.push_back(Process(pid));
-        }
-
-  std::vector<Process>::const_iterator first = processes.end() -n ;
-  std::vector<Process>::const_iterator last = processes.end();
-  std::vector<Process> subProc(first, last);
-    std::sort(processes.begin(),processes.end());
-    std::reverse(processes.begin(),processes.end());
-  System system;
-  std::vector<Process> testpro;
-  testpro.push_back(3867);
-  system.SetProcesses(processes);
+ int n = 20;
+ std::vector<int> pidVector = LinuxParser::Pids();
+ for (auto pid:pidVector){
+   processes.push_back(Process(pid));
+  }
+ std::sort(processes.begin(),processes.end());
+ std::reverse(processes.begin(),processes.end());
+ System system;
+ std::vector<Process> testpro;
+ testpro.push_back(3867);
+ system.SetProcesses(processes);
  NCursesDisplay::Display(system,n);
-
 }
